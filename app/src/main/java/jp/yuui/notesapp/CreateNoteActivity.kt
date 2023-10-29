07614e067d1151b81958365606032d9ce974694e
Yuurei <yuureivt@gmail.com>
2023-10-28 17:43:42 -0700

    Base Files

---
+++ app/src/main/java/jp/yuui/notesapp/CreateNoteActivity.kt
@@ -0,0 +1,16 @@
+package jp.yuui.notesapp
+
+import android.os.Bundle
+import androidx.fragment.app.FragmentActivity
+import jp.yuui.notesapp.databinding.CreateNoteActivityBinding
+
+class CreateNoteActivity: FragmentActivity() {
+    private var _binding: CreateNoteActivityBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        _binding = CreateNoteActivityBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
